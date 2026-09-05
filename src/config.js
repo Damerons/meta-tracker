85b41c51684aa490a8323e085c8328a1029dfd14
Damerons <185724390+Damerons@users.noreply.github.com>
2026-09-05 00:32:09 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -13,7 +13,10 @@ export const SITE_CONFIG = {
     "www.luminresearch.shop",
     "shop.luminresearch.shop",
     "www.shop.luminresearch.shop",
-
+    "lumingrade.com",
+    "www.lumingrade.com",
+    "luminstandard.com",
+    "www.luminstandard.com",
     // Add additional legitimate subdomains here:
     // "shop.opensciencemuseum.org",
     // "us.opensciencemuseum.org",
@@ -55,7 +58,7 @@ export const PATH_MAPPINGS = {
   "/products/glp3-rt": "/shop/advanced-botanical-materials-slide-collection/",
   "/products/glp2-tz": "/shop/optical-resolution-test-slide-set/",
   "/products/glp1-sm": "/shop/historical-microscopy-observation-kit/",
-  "/checkout/order-recieved": "/thank-you",
+  "/checkout/order-received": "/thank-you",
   "/order-confirmed": "/thank-you",
   "/order-confirmed/thank-you-page-2": "/thank-you",
   "/privacy-policy": "/privacy",
